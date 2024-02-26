28cecf7a875ab67b2121550f607a3099b49499d9
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-02-26 17:52:42 +0100

    Obdelnik s Getter a Setter

---
+++ Po1640_Obdelnik/Model/Obdelnik2.cs
@@ -0,0 +1,41 @@
+﻿namespace Po1640_Obdelnik.Model
+{
+    public class Obdelnik2
+    {
+        private int stranaA;
+        private int stranaB;
+
+        public int StranaA
+        {
+            get => stranaA; 
+            set
+            {
+                if (stranaA != value)
+                {
+                    if(value < 0)
+                        stranaA = 0;
+                    else 
+                        stranaA = value;
+                }
+            }
+        }
+        public int StranaB
+        {
+            get => stranaB; set
+            {
+                if (stranaB != value)
+                {
+                    if (value < 0)
+                        stranaB = 0;
+                    else
+                        stranaB = value;
+                }
+            }
+        }
+        public int Obvod => 2 * (StranaA + StranaB);
+        public int Obsah => StranaA * StranaB;
+        public double Uhlopricka => Math.Sqrt(StranaA * StranaA + Math.Pow(StranaB, 2));
+
+
+    }
+}
